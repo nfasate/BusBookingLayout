c117300159fea45ff2057b5026edb36acdce90fc
ptejas26 <ptejas.patelia@gmail.com>
2018-07-05 06:46:23 +0530

    Initial Commit

---
+++ BusBookingLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusBookingLayout
+//
+//  Created by Nilesh's MAC on 7/5/18.
+//  Copyright © 2018 Nilesh's MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
